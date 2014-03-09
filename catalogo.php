b6e5ebe66e256ad7ee27db017c3c9a4eb3bb8fff
Angel Malavar <cyberman68@hotmail.com>
2014-03-09 11:09:43 -0600

    nuevo archivo catalogo
    Aqui se va a mostrar todo el contenido de busquedas y el catalago de
    celulares disponible

--- catalogo.php
+++ catalogo.php
@@ -36,8 +36,8 @@
             </div>
             <div class="navbar-collapse collapse">
               <ul class="nav navbar-nav">
-                <li class="active"><a href="index.php">Inicio</a></li>
-                <li><a href="catalogo.php">Catálogo</a></li>
+                <li><a href="index.php">Inicio</a></li>
+                <li class="active"><a href="catalogo.php">Catálogo</a></li>
                 <li><a href="labs.php"><span class="glyphicon glyphicon-shopping-cart"></span>&nbspVer Carrito</a></li>
               </ul>
               <form class="navbar-form navbar-right">
