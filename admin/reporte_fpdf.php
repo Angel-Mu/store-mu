6ae8af6f6ed72773fee5379889d779c952799c1b
Ricardocf <cfro113365@upemor.edu.mx>
2014-03-10 01:06:30 -0600

    reporte pdf generado con solo texto

--- admin/reporte_fpdf.php
+++ admin/reporte_fpdf.php
@@ -27,18 +27,20 @@ $pdf=new PDF();
 $pdf->AliasNbPages(); 
 $pdf->AddPage(); 
 
-$consulta = mysql_query("select marca, modelo, serie from celular where stock < 15");
-$pdf->Cell(30,6,"Marca",1,0,'L');
-$pdf->Cell(30,6,"Modelo",1,0,'L');
-$pdf->Cell(40,6,"Serie",1,0,'L');
+$consulta = mysql_query("select marca, modelo, serie, stock from celular where stock < 25");
+$pdf->Cell(50,6,"Marca",1,0,'L');
+$pdf->Cell(50,6,"Modelo",1,0,'L');
+$pdf->Cell(38,6,"Serie",1,0,'L');
+$pdf->Cell(20,6,"Stock",1,0,'L');
 //$pdf->Cell(18,6,"Imagen",1,0,'L');
 $pdf->Cell(15,6,"",0,1,'L');
 while($resultado = mysql_fetch_array($consulta)){ 
  
 #$pdf->Cell(17,20,$resultado['categoria'],1,0,'L'); 
-$pdf->Cell(30,10,$resultado['marca'],1,0,'L');
-$pdf->Cell(30,10,$resultado['modelo'],1,0,'L');
-$pdf->Cell(40,10,$resultado['serie'],1,0,'L');
+$pdf->Cell(50,10,$resultado['marca'],1,0,'L');
+$pdf->Cell(50,10,$resultado['modelo'],1,0,'L');
+$pdf->Cell(38,10,$resultado['serie'],1,0,'L');
+$pdf->Cell(20,10,$resultado['stock'],1,0,'L');
 //$pdf->Cell(50,25, $pdf->Image($resultado['imagen'], $pdf->GetX(),$pdf->GetY(),17),1,0,'C');
 $pdf->Ln(); 
 } 
