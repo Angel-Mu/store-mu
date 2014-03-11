6f0a7dca86b07f5587c12a835f91fdc320da0a71
Angel Malavar <cyberman68@hotmail.com>
2014-03-11 01:41:16 -0600

    modificando el carrito del index

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
               <ul class="nav navbar-nav">
                 <li class="active"><a href="index.php">Inicio</a></li>
                 <li><a href="catalogo.php">Catálogo</a></li>
-                <li><a href="carrito.php"><span class="glyphicon glyphicon-shopping-cart">&nbspVer Carrito</span></a></li>
+                <li><a href="carrito.php"><span class="glyphicon glyphicon-shopping-cart"></span>&nbspVer Carrito</a></li>
               </ul>
               <form class="navbar-form navbar-right">
                 <div class="form-group">
