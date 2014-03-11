5b890973887222d1a363f9478ff9c584aa171198
Angel Malavar <cyberman68@hotmail.com>
2014-03-11 00:55:40 -0600

    Ya actualiza el stock puto hahah

--- comprar.php
+++ comprar.php
@@ -8,10 +8,17 @@
     $consUser = mysql_query("select * from comprador order by id_comprador DESC;");
     $user=mysql_fetch_array($consUser);
     $idComprador=$user['id_comprador'];
-    echo $cant_pagar;
     $insertCompra = mysql_query("insert into ticket(id_comprador,id_carrito,total_pago) values($idComprador,$cart,$cant_pagar);");
     if(!$insertCompra){
     	echo "ERROR al insertar ticket";
-    } 
+    }
+    $consCarro = mysql_query("select * from carrito where id_carrito='".$cart."';");
+    while($lstCar=mysql_fetch_array($consCarro)){
+    	echo $lstCar['cantidad']." ".$lstCar['id_carrito'];
+    	$actStock=mysql_query("update celular set stock=stock-".$lstCar['cantidad']." where id_celular=".$lstCar['id_celular']);
+    	if(!$actStock){
+	    	echo "ERROR al actualizar";
+	    }
+    }
 ?>
  
